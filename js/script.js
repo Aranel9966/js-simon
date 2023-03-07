07e35d05332eea35520bb0d6ab5a546590285112
Aranel9966 <99angelo.dire@gmail.com>
2023-03-07 18:26:37 +0100

    add for control

--- js/script.js
+++ js/script.js
@@ -15,14 +15,15 @@ Gestire l'inserimento dei numeri tramite 5 input diversi.
     ->timer 10 secondi e poi nascondi 
 ->creo 5 imput diversi che compaiono al 11 secondo
 ->dopo l'inserimento dei numeri 
-->controllo di quanti e quali numeri sono stati indocinati
+->controllo di quanti e quali numeri sono stati indovinati
 */
 
-newRandom = [];
+let newRandom = [];
 let userNumber = [];
 let numberRandom = document.getElementById('number-random');
 let containerInput = document.getElementById('container-input');
 let startEl = document.getElementById('start');
+let resultsEl = document.getElementById('results');
 
 containerInput.style.display='none';
 randomNumber();
@@ -31,6 +32,7 @@ createInput(containerInput)
 let firstInterval = setTimeout(removeNumber,1000);
 let secondInterval = setTimeout(displayInput,1000);
 
+// evento che salva i valori in input in un array 
 startEl.addEventListener('click',function(){
     userNumber = [];
 
@@ -39,11 +41,32 @@ startEl.addEventListener('click',function(){
     }
     
     console.log(userNumber)
+    console.log(newRandom)
 
+// for annidato per controllare se i numeri sono uguali
+// let nNumberEquals = 0;
+    for(let i = 0; i < newRandom.length; i++){
+        let number = newRandom[i];
 
+        for(let j=0; j< userNumber.length;j++){
+            let numberUser = userNumber[i];
+            
+            
+            if(newRandom == numberUser ){
+                // nNumberEquals++
+                resultsEl.innerHTML= number
+                console.log('giusti')
+                
+            }else{
+
+                // console.log(number)
+            }
+        }
+        console.log(number)
+    
+    }
+    
 })
-
-
 
 
 
@@ -53,12 +76,15 @@ startEl.addEventListener('click',function(){
 // function
 ///////////////////////////////////////////////////////////////
 
+//confronta quanti e quali numeri sono stati indovinati
+function controlNumber(){
+    
+}
 
 // crea 5 numeri casuali 
 function randomNumber() {
     
     for(let i = 0; i < 5; i++){
-
         newRandom.push(Math.floor(Math.random() * (99 - 1 + 1) + 1));
         
     }
